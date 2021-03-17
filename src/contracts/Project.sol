64a762f9032eb13c82fbddd448d944bbf3297a10
DanyAlej <da.benavides@uniandes.edu.co>
2021-03-17 18:22:47 -0500

    Better test and a contract for projects:

--- src/contracts/Project.sol
+++ src/contracts/Project.sol
@@ -1,61 +1,49 @@
-// Solidity files have to start with this pragma.
-// It will be used by the Solidity compiler to validate its version.
 pragma solidity ^0.7.0;
 
-
-// This is the main building block for smart contracts.
 contract Project {
-    // Some string type variables to identify the token.
-    // The `public` modifier makes a variable readable from outside the contract.
-    string public name = "CheapEthereum";
-    string public symbol = "cETH";
 
-    // The fixed amount of tokens stored in an unsigned integer type variable.
-    uint256 public totalSupply = 1000000;
+    string public charityName;
+    string public description;
 
-    // An address type variable is used to store ethereum accounts.
-    address public owner;
+    //How much the project want's to raise
+    uint public goal;
 
-    // A mapping is a key/value map. Here we store each account balance.
-    mapping(address => uint256) balances;
+    address payable public charityAdress;
 
-    /**
-     * Contract initialization.
-     *
-     * The `constructor` is executed only once when the contract is created.
-     */
+    mapping (address => uint) public donations;
+    address[] public donators;
+
+    uint public currentAmount;
+
+    enum State { Created, Started, Funded, Inactive }
+
+    State public state;
+    
     constructor() {
-        // The totalSupply is assigned to transaction sender, which is the account
-        // that is deploying the contract.
-        balances[msg.sender] = totalSupply;
-        owner = msg.sender;
+
+        charityAdress = msg.sender;
+        state = State.Started;
+
     }
 
-    /**
-     * A function to transfer tokens.
-     *
-     * The `external` modifier makes a function *only* callable from outside
-     * the contract.
-     */
-    function transfer(address to, uint256 amount) external {
-        // Check if the transaction sender has enough tokens.
-        // If `require`'s first argument evaluates to `false` then the
-        // transaction will revert.
-        require(balances[msg.sender] >= amount, "Not enough tokens");
+    function donate(uint256 amount) payable external {
+        require(msg.value >= 0, "Not enough tokens");
+        require(state == State.Started, "Project not started or already funded");
 
         // Transfer the amount.
-        balances[msg.sender] -= amount;
-        balances[to] += amount;
+        donations[msg.sender] += msg.value;
+        donators.push(msg.sender);
+
+        currentAmount += msg.value;
+
     }
 
-    /**
-     * Read only function to retrieve the token balance of a given account.
-     *
-     * The `view` modifier indicates that it doesn't modify the contract's
-     * state, which allows us to call it without executing a transaction.
-     */
-    function balanceOf(address account) external view returns (uint256) {
-        return balances[account];
+    function totalDonated() external view returns (uint256) {
+        return currentAmount;
+    }
+
+    function getNumberOfDonors() external view returns (uint256) {
+        return donators.length;
     }
 }
 
