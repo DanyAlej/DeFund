fa75cc35b7a3b5e3fe600b4dc56366ce9a4faf83
DanyAlej <da.benavides@uniandes.edu.co>
2021-04-02 21:11:43 -0500

    Frontend skeleton and start of integration of frontend and hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,10 @@
+require("@nomiclabs/hardhat-ethers");
+require("hardhat-deploy-ethers");
+require("hardhat-deploy");
+require("@symfoni/hardhat-react");
+require("hardhat-typechain");
+require("@typechain/ethers-v5");
+
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-web3");
 /**
---
+++ src/defund-react/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> DeFund: Decentralized funding for charities projects </h1>
+    </div>
+  );
+}
+
+export default App;
